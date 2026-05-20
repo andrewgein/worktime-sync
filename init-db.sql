9a78ae41f16306c50ef080f891b38ef003da49f1
rahmedovdamir <rahmedov17@mail.ru>
2026-05-20 21:16:31 +0300

    add team_service

--- init-db.sql
+++ init-db.sql
@@ -1,2 +1,3 @@
 SELECT 'CREATE DATABASE mail' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mail')\gexec
 SELECT 'CREATE DATABASE profile_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'profile_db')\gexec
+SELECT 'CREATE DATABASE team_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'team_service')\gexec
