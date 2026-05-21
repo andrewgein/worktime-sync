9d006506454a76e8f49514359368abc43779062f
rahmedovdamir <rahmedov17@mail.ru>
2026-05-21 14:24:44 +0300

    add task-service

--- init-db.sql
+++ init-db.sql
@@ -1,3 +1,4 @@
 SELECT 'CREATE DATABASE mail' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mail')\gexec
 SELECT 'CREATE DATABASE profile_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'profile_db')\gexec
 SELECT 'CREATE DATABASE team_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'team_service')\gexec
+SELECT 'CREATE DATABASE task_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'task_db')\gexec
